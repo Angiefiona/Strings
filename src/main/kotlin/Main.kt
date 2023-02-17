30307e9518722cd923b641d972ca930a7a90a86f
Angiefiona <angellamumoh@gmail.com>
2023-02-17 11:45:02 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,80 @@
+fun main() {
+    var txt = "Akirachix"
+    println(txt[0]+" "+txt[2]+" "+txt[3])
+
+    var x = "Hi my name is Mumo"
+    var y = "I am 12 years old"
+    println(x + " "+ y)
+
+    var county = "Turkana"
+    println("The length of county string is: " + county.length)
+
+    var name = "Angela"
+    var name2 = "Fiona"
+    if(name.equals(name2)){
+        println("That's me!")}else{
+            println("I don't know who that is!")}
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
